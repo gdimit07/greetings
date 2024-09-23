129616930f2cc839f06fd327b4b90036aa1d4bea
George Demetriades <georgedemetriades@Georges-MacBook-Air.local>
2024-09-23 16:58:49 +0100

    New test cases

--- greetings_test.go
+++ greetings_test.go
@@ -15,3 +15,21 @@ func TestHelloName(t *testing.T) {
 	assert.Contains(t, msg, customMessage)
 	assert.Nil(t, err)
 }
+
+func TestHelloEmptyName(t *testing.T) {
+	name := ""
+	customMessage := "fake custom"
+	msg, err := Hello(name, customMessage)
+
+	assert.Equal(t, msg, name)
+	assert.Error(t, err)
+}
+
+func TestHelloEmptyCustomMessage(t *testing.T) {
+	name := "Bob"
+	customMessage := ""
+	msg, err := Hello(name, customMessage)
+
+	assert.Contains(t, msg, name)
+	assert.Nil(t, err)
+}
